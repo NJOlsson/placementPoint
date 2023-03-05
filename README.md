33a77631bb27d2f11bff71d6fff6419b976dcdac
Johannes Olsson <nils.johannes.olsson@proton.me>
2023-03-05 22:12:22 +0100

    Copy code from assignment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # placementPoint
+
+## How to set up local development
+```sh
+
+# Install dependencies
+npm i
+
+# Start the app and open it in your browser
+npm run dev -- --open
+```
+
+## Limitations
+ - Can end up in local maximums and miss global maximum.
+ - No validation is done on input
+
+I decided to try writing this in javascript. Something I regret in hindsight. I am just not used to it and had to spend a lot of time fighting against the language.
+
+All files were copied from https://github.com/finch3d/finch-assignment-algo except for src/geometryUtils.js and src/getPlacementPoint.js which I wrote.
+
+If I had more time I would want experiment with [Voroni Diagrams](https://en.wikipedia.org/wiki/Voronoi_diagram)
---
+++ src/Floorplan.jsx
@@ -0,0 +1,61 @@
+import './Floorplan.css';
+import { getViewBoxForPolygon } from './getViewBoxForPolygon.js';
+import { getPlacementPoint } from './getPlacementPoint'
+
+export function Floorplan({ polygon, label }) {
+  const viewBox = getViewBoxForPolygon(polygon, { padding: 1000 });
+
+  // Right now, we're just placing the label by the first point in the polygon.
+  // This clearly isn't very good, so replace this with your own logic to figure
+  // out an optimal position for it within each polygon.
+  const labelPosition = getPlacementPoint(polygon);
+
+  return (
+    <div className="Floorplan">
+      <Image viewBox={viewBox} polygon={polygon} />
+      <Label viewBox={viewBox} position={labelPosition}>
+        {label}
+      </Label>
+    </div>
+  );
+}
+
+function Image({ viewBox: { left, top, width, height }, polygon }) {
+  return (
+    <svg
+      width={400}
+      height={400}
+      viewBox={`${left} ${top} ${width} ${height}`}
+      className="Floorplan__image"
+    >
+      <path
+        fillRule="evenodd"
+        fill="white"
+        stroke="#777"
+        strokeWidth={2}
+        vectorEffect="non-scaling-stroke"
+        d={polygon.polylines.map(
+          ({ points: [firstPoint, ...restPoints] }) =>
+            `M ${firstPoint[0]} ${firstPoint[1]} ${restPoints
+              .map(([x, y]) => `L ${Math.round(x)},${Math.round(y)}`)
+              .join(' ')}`
+        )}
+      />
+    </svg>
+  );
+}
+
+function Label({ viewBox: { left, top, width, height }, position, children }) {
+  return (
+    <div
+      className="Floorplan__label"
+      style={{
+        // Negate the vertical position to have the Y axis point up
+        top: `${(1 - (position[1] - top) / height) * 100}%`,
+        left: `${((position[0] - left) / width) * 100}%`,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
